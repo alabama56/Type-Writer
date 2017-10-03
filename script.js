2d3c43bb3322383ea6a60432bd55e0b3289dbea0
alabama56 <jakedownard@yahoo.com>
2017-10-03 17:03:58 -0500

    used jquery to get the first half of the lab done

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 var key = document.getElementsByClassName("key")
-var myId = $(key).attr('id');
-var id = Number(myId);
-// var keyId = span.id;
-// // var id = Number(keyId)
+var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+var num = 0;
 
+var sentenceCounter = 0;
+var characterCounter = 0;
+var sentenceDiv = $("#sentence");
+var targetDiv = $("#target-letter")
+var n = sentences[sentenceCounter].charAt(num++);
+var span = $("span")
+   
 $(document).ready(function(){
     $("#keyboard-upper-container").hide();
+    
+    $("<span class='glyphicon glyphicon-ok'></span>").appendTo("#feedback");
+    $("<span class='glyphicon glyphicon-remove'></span>").appendTo("#feedback");
+    $(".glyphicon-ok").hide();
+    $(".glyphicon-remove").hide();
+    setSentence();
+    promptHighlight();
+    
     $(document).keydown(function(e){
         if(e.which === 16) {
             $("#keyboard-upper-container").show();
@@ -44,7 +58,67 @@ $(document).ready(function(){
            }
         }
    });
-   
-    
 
+   $(targetDiv).text(n);
+   
+   $(document).keypress(function (e){
+      
+      var x = String.fromCharCode(e.which);
+        if(x === n){
+            n = sentences[0].charAt(num++);
+            $(targetDiv).text(n);
+            characterCounter++;
+            promptHighlight();
+            
+           
+            
+            glyphOK();
+          
+        }else if(x != n){
+            glyphRemove();
+        }
+        if($("#sentence, span").is(":last-child")){
+            alert("hell");
+            // sentenceCounter = 1;
+            // setSentence();
+        }
+       
+
+        
+   
+   });
 });
+
+
+
+function promptHighlight(){
+    $("#letter-"+characterCounter).css("background-color", "yellow");
+    if (characterCounter!=0) {
+        $("#letter-"+(characterCounter-1)).css("background-color", "white");
+    }
+}
+
+function setSentence(){
+    var sentenceString = sentences[sentenceCounter];
+    var sentenceSplit = sentenceString.split("");
+    for (var i = 0; i<=sentenceString.length-1; i++) {
+        $("<span id=letter-"+i+">"+sentenceSplit[i]+"</span>").appendTo(sentenceDiv);
+        
+    }
+}
+
+function glyphOK(){
+    $(".glyphicon-ok").show();
+    $(".glyphicon-remove").hide();
+}
+
+function glyphRemove(){
+    $(".glyphicon-remove").show();
+    $(".glyphicon-ok").hide();
+}
+
+function noGlyph() {
+    $(".glyphicon-ok").hide();
+    $(".glyphicon-remove").hide();
+}
+
